b5ac553716b83f89e07833a716d22d3941a8da10
zagovorichev <zagovorichev@gmail.com>
2019-07-18 13:18:56 +0300

    LtiUser Data Provider

--- model/dataProvider/SyncClientDataProviderService.php
+++ model/dataProvider/SyncClientDataProviderService.php
@@ -44,9 +44,9 @@ class SyncClientDataProviderService extends ConfigurableService implements SyncC
     {
         $data = [];
         foreach ($this->getGroupedTasks($tasks) as $type => $items) {
-            // if ($type === 'test_session') {
+            if ($type === 'lti_user') {
                 $data[$type] = $this->getProvider($type)->getData($items);
-            // }
+            }
         }
         return $data;
     }
